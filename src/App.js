4effad15eb598291bf73a219fa782158ad6035ed
alessio_cuoccio <alessio.cuoccio@outlook.com>
2021-11-19 10:36:18 +0100

    project finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-  const [cartIsShown, setCartIsShown] =useState(false);
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-  function showCartHandler() {
-    setCartIsShown = true;
-  }
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
 
-  function hideCartHandler() {
-    setCartIsShown = false;
-  }
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
 
   return (
     <CartProvider>
-      
-      {cartIsShown && <Cart onClose = {hideCartHandler}/>}
-      <Header onShowCart = {showCartHandler} />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
